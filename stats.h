bcdbb95b9ca0a5d72228f02f50ddd07b0c101e64
Rajshree208 <46247383+Rajshree208@users.noreply.github.com>
2020-09-03 12:58:02 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,11 +2,18 @@
 
 namespace Statistics {
     class Stats
-    {
-        public:
-            float average;
-            float min;
-            float max;
-    }
-    Stats ComputeStatistics(const std::vector<float>& );
+	{
+	private:
+		float average;
+		float min;
+		float max;
+	public:
+		Stats(float average, float min, float max)
+		{
+			this->average = average;
+			this->min = min;
+			this->max = max;
+		}
+	};
+	Stats ComputeStatistics(const std::vector<float>& numbers);
 }
