48910cf90562aea3387567121f2157ba68a1544c
Rajshree208 <46247383+Rajshree208@users.noreply.github.com>
2020-09-03 10:46:06 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,42 @@
 #include "stats.h"
+#include <vector>
+float getAverage(const std::vector<float> numbers)
+{
+	float sum = 0.0;
+	for(float value:numbers)
+	{
+		sum += value;
+	}
+	return (sum / numbers.size());
+}
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+float getMin(const std::vector<float> numbers)
+{
+	float minValue = numbers[0];
+	for(float value:numbers)
+	{
+		if (value < minValue)
+			minValue = value;
+	}
+	return minValue;
+}
+
+float getMax(const std::vector<float> numbers)
+{
+	float maxValue = numbers[0];
+	for (float value : numbers)
+	{
+		if (value < maxValue)
+			maxValue = value;
+	}
+	return maxValue;
+}
+
+Stats Statistics::ComputeStatistics(const std::vector<float>& numbers)
+{
+	Stats statistics;
+	statistics.average = getAverage(numbers);
+	statistics.min = getMin(numbers);
+	statistics.max = getMax(numbers);
+    return statistics;
 }
